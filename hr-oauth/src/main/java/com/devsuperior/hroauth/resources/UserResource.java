2fad96b15640f477734d75ce1bf764f5394f00b8
felipehogrefe <felipehogrefe@gmail.com>
2021-12-16 15:25:42 -0300

    Configures hr-oauth tu use the feign client in order to access the user service

---
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/resources/UserResource.java
@@ -0,0 +1,27 @@
+package com.devsuperior.hroauth.resources;
+
+import com.devsuperior.hroauth.entities.User;
+import com.devsuperior.hroauth.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<User> findByEmail(@RequestParam String email){
+        try {
+            return ResponseEntity.ok(userService.findByEmail(email));
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+}
