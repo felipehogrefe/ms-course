d8e1e63d7307b61411507bab3b5e33f8a184461a
felipehogrefe <felipehogrefe@gmail.com>
2021-12-16 22:29:00 -0300

    Fix some typos

--- hr-user/src/main/java/com/devsuperior/hruser/resources/UserResource.java
+++ hr-user/src/main/java/com/devsuperior/hruser/resources/UserResource.java
@@ -1,33 +1,32 @@
 package com.devsuperior.hruser.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.devsuperior.hruser.entities.User;
 import com.devsuperior.hruser.repositories.UserRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.core.env.Environment;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-@RefreshScope
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserRepository repository;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id){
-        return ResponseEntity.ok( userRepository.findById(id).get());
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = repository.findById(id).get();
+        return ResponseEntity.ok(obj);
     }
 
     @GetMapping(value = "/search")
-    public ResponseEntity<User> findByEmail(@RequestParam String email){
-        return ResponseEntity.ok( userRepository.findByEmail(email));
+    public ResponseEntity<User> findByEmail(@RequestParam String email) {
+        User obj = repository.findByEmail(email);
+        return ResponseEntity.ok(obj);
     }
 }
